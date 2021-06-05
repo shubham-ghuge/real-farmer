9dce1e8526eb98db41c44e01a579b6378aee5473
shubham-ghuge <shubhamghuge34@gmail.com>
2021-06-05 14:56:22 +0530

    added Authentication on front end

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import "../utils.css";
 import { Routes, Route } from "react-router-dom";
+import { PrivateRoute } from "../PrivateRoute/PrivateRoute";
 import Home from "../../pages/Home";
 import Quiz from "../../pages/Quiz";
 import Dashboard from "../../pages/Dashboard";
@@ -9,9 +10,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz/:quizId" element={<Quiz />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home />} />``
+        <PrivateRoute path="/quiz/:quizId" element={<Quiz />} />
+        <PrivateRoute path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
